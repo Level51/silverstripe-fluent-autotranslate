a5e0f4957932b741f280df0589c9e4a90db66eb3
b.broethaler <b.broethaler@pixelpoems.com>
2024-10-03 11:44:48 +0200

    Added Option to Use OpenAi for Translations

---
+++ src/BlackListExtension.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Level51\Autotranslate;
+
+use SilverStripe\Forms\ReadonlyField;
+use SilverStripe\Forms\TextareaField;
+use SilverStripe\ORM\DataExtension;
+
+class BlackListExtension extends DataExtension
+{
+    private static $db = [
+        'BlackListValues' => 'Text'
+    ];
+
+    public function updateCMSFields($fields)
+    {
+        $fields->addFieldsToTab('Root.Translation', [
+            TextareaField::create('BlackListValues', 'BlackList Values')
+                ->setDescription('Enter one value per line, the translatior will ignore these values and keep them as they are.'),
+            ReadonlyField::create('BlackListValuesPreview', 'Preview')
+                ->setValue($this->BlackListValues())
+                ->setDescription('You have to reload the page to see the changes.')
+        ]);
+    }
+
+    public function BlackListValues(): string
+    {
+        if(!$this->owner->BlackListValues) return '';
+
+        $values = str_replace("\r", "", $this->owner->BlackListValues);
+        $values = explode("\n", $values);
+        return implode(", ", $values);
+    }
+
+}
